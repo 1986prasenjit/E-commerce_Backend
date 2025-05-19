a0d4096e4c0ab29be6b10acd535734f83d82a571
1986prasenjit <prasenjitbasu44@gmail.com>
2025-05-19 12:32:29 +0530

    added initial project step with minimum packages

---
+++ backend/src/index.js
@@ -0,0 +1,19 @@
+import express from "express";
+
+import dotenv from "dotenv";
+
+dotenv.config({
+    path:"./.env"
+})
+
+const app = express();
+
+const PORT = process.env.PORT || 5000
+
+app.get("/", (req, res)=> {
+    res.send("Hello from the Server")
+})
+
+app.listen(PORT, ()=> {
+    console.log(`Server is listening at PORT Number ${PORT}`);
+})
